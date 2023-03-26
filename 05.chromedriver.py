be979284e3dc9cccc67de06ae850663f4ecfd5f0
chenhang233 <1973433180@qq.com>
2023-03-26 20:00:40 +0800

    chromedriver

---
+++ 05.chromedriver.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+# 下载 chromedriver(看版本) 放到 D:\Python\Tools\scripts 
+# pip install selenium
+diver = webdriver.Chrome()
+
+
+diver.get("http://192.168.232.130:7001")
+time.sleep(3)
+diver.find_element(by=By.ID,value="username").clear()
+time.sleep(1)
+diver.find_element(by=By.ID,value="username").send_keys("test")
+time.sleep(1)
+diver.find_element(by=By.XPATH,value="//*[@id='root']/div/div/section/div/form/div[3]/div/div/div/div/div/div[1]/button").click()
+
+time.sleep(5)
